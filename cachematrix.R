350b8378a05a02dfbb1623c447265af526147936
Fulio <hua.francois@gmail.com>
2016-08-24 21:58:02 +0200

    adds

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,56 @@
 ## functions do
 
 ## Write a short comment describing this function
+makeVector <- function(x = numeric()) {
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setmean <- function(mean) m <<- mean
+    getmean <- function() m
+    list(set = set, get = get,
+         setmean = setmean,
+         getmean = getmean)
+}
 
 makeCacheMatrix <- function(x = matrix()) {
+    ## cache the inverse matrix of x
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(solve) m <<- solve
+    getInverse <- function() m
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 
 }
 
 
 ## Write a short comment describing this function
-
+cachemean <- function(x, ...) {
+    m <- x$getmean()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- mean(data, ...)
+    x$setmean(m)
+    m
+}
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m <- x$getInverse()
+    if(!isnull(m)) {
+        message("getting cached inverse matrix")
+        return(m)
+    }
+    data <-x$get
+    m <- solve(data)
+    x$setInverse(m)
+    m
 }
