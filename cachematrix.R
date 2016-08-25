80b15a0d7b849b947049c9a10d204c10f0adb96d
Fulio <hua.francois@gmail.com>
2016-08-25 22:40:21 +0200

    add comments

--- cachematrix.R
+++ cachematrix.R
@@ -2,16 +2,19 @@
 ## functions do
 
 ## Write a short comment describing this function
-## makeCacheMatrix takes a square matrix as argument, caching the
+## makeCacheMatrix takes an invertible matrix as argument, caching the
 ## matrix and binding setters and getters for the matrix and the inverted matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-    ## cache the inverse matrix of x
+    ## set invertedMatrix to NULL
     m <- NULL
+    ## sets the invertibleMatrix value and the invertedMatrix to NULL 
+    ## (if the value of the matrix changes its inverted matrix changes too)
     set <- function(y) {
         x <<- y
         m <<- NULL
     }
+    ## getter
     get <- function() x
     setInverse <- function(solve) m <<- solve
     getInverse <- function() m
