8488589ced4addff76ec04cabd6e4fb13aac5aea
Fulio <hua.francois@gmail.com>
2016-08-25 07:40:54 +0200

    working copy, TODO : rename variables to something meaningfull

--- cachematrix.R
+++ cachematrix.R
@@ -2,19 +2,8 @@
 ## functions do
 
 ## Write a short comment describing this function
-makeVector <- function(x = numeric()) {
-    m <- NULL
-    set <- function(y) {
-        x <<- y
-        m <<- NULL
-    }
-    get <- function() x
-    setmean <- function(mean) m <<- mean
-    getmean <- function() m
-    list(set = set, get = get,
-         setmean = setmean,
-         getmean = getmean)
-}
+## makeCacheMatrix takes a square matrix as argument, caching the
+## matrix and binding setters and getters for the matrix and the inverted matrix
 
 makeCacheMatrix <- function(x = matrix()) {
     ## cache the inverse matrix of x
@@ -32,25 +21,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-cachemean <- function(x, ...) {
-    m <- x$getmean()
-    if(!is.null(m)) {
-        message("getting cached data")
-        return(m)
-    }
-    data <- x$get()
-    m <- mean(data, ...)
-    x$setmean(m)
-    m
-}
+
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
     m <- x$getInverse()
-    if(!isnull(m)) {
+    if(!is.null(m)) {
         message("getting cached inverse matrix")
         return(m)
     }
-    data <-x$get
+    data <-x$get()
     m <- solve(data)
     x$setInverse(m)
     m
